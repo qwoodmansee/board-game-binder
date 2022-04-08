cf577fffde0309297fd3e87db56067a84c6b8ec8
Quinton Woodmansee <qwoodmansee@capital.edu>
2022-04-08 00:05:21 -0400

    up atlas to max count for API

--- src/network/atlas-integration.js
+++ src/network/atlas-integration.js
@@ -2,8 +2,8 @@ import fetch from "node-fetch";
 
 export const fetchBoardGamesFromAtlas = async (jsonGameInputData) => {
   const allIdString = jsonGameInputData.map((g) => g.atlasId).join(",");
-  const requestString = `https://api.boardgameatlas.com/api/search?ids=${allIdString}&client_id=${process.env.ATLAS_CLIENT_ID}`;
-
+  // can only request 100 atlas games
+  const requestString = `https://api.boardgameatlas.com/api/search?ids=${allIdString}&client_id=${process.env.ATLAS_CLIENT_ID}&limit=100`;
   let responseJson;
   try {
     const result = await fetch(requestString);
